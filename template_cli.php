911cf51d367ad5b0c080b0550ccfed7b8836c44e
Lionel POINTET <l.Feucophe@free.fr>
2012-10-07 21:14:40 +0200

    Bugfix - Meilleure presentation des erreurs en CLI

--- template_cli.php
+++ template_cli.php
@@ -6,6 +6,6 @@ if(!empty($param) && !isset($message['fatal']) && !empty($message['warning'])) {
 
 // Il y a eu des erreurs ou aucun paramètre passé
 if(!empty($message['fatal'])) {
-    echo implode(PHP_EOL, $message['fatal']);
+    echo PHP_EOL . str_replace(array('&laquo;', '&raquo;'), '"', implode(PHP_EOL, $message['fatal'])) . PHP_EOL . PHP_EOL;
 }
 echo STR_CLI_USAGE;
